bd415a3c3d73fe35a8964c140fa9cd19d9125a80
xxchaosxx210 <pmillar1279@googlemail.com>
2026-09-25 07:15:52 +0100

    Clean dummy test downloads after completion or cancel

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -2,6 +2,7 @@ import wx
 
 import crawler.message as const
 import crawler.options as options
+import crawler.testoptions as testoptions
 import gui.notificationbar as notify
 
 from gui.downloadpanel import DownloadPanel
@@ -24,6 +25,8 @@ class MainWindow(wx.Frame):
         self._load_icon()
         self.SetMenuBar(PixGrabberMenuBar(parent=self))
         self._fetch_dlg = None
+        self._dummy_test_active = False
+        self._dummy_test_save_path = None
 
         self.dld_panel = DownloadPanel(parent=self)
         vs = wx.BoxSizer(wx.VERTICAL)
@@ -154,6 +157,12 @@ class MainWindow(wx.Frame):
                     self.detached_frame.Hide()
 
     def _on_fetch_start(self, msg: const.Message):
+        self._dummy_test_active = testoptions.is_test_url(msg.data["url"])
+        if self._dummy_test_active:
+            self._dummy_test_save_path = options.load_settings().get("save_path", "")
+        else:
+            self._dummy_test_save_path = None
+
         timer_quit.clear()
         create_timer_thread(self._on_timer_callback).start()
         self.dld_panel.progressbar.reset_progress(10)
@@ -177,13 +186,30 @@ class MainWindow(wx.Frame):
     
     def _on_scraping_complete(self):
         self.detached_frame.Hide()
+
+        test_downloads_deleted = False
+        if self._dummy_test_active:
+            test_settings = testoptions.load_test_settings()
+            if test_settings["delete_downloads_after_test"] and self._dummy_test_save_path:
+                try:
+                    test_downloads_deleted = testoptions.cleanup_test_downloads(
+                        self._dummy_test_save_path
+                    )
+                except OSError:
+                    test_downloads_deleted = False
+            self._dummy_test_active = False
+            self._dummy_test_save_path = None
+
         # play the notification sound if required
         if options.load_settings()["notify-done"]:
             self.app.sounds["complete"].Play()
             notify.NotificationBar(None, -1, "", "PixGrabber has completed", timeout=notify.NOTIFY_LONG)
         # kill the timer thread
         timer_quit.set()
-        self.SetStatusText("All Tasks have completed")
+        if test_downloads_deleted:
+            self.SetStatusText("All Tasks have completed - test downloads deleted")
+        else:
+            self.SetStatusText("All Tasks have completed")
         self.dld_panel.progressbar.reset_progress(0)
         self.dld_panel.addressbar.txt_address.SetValue("")
         self.dld_panel.enable_controls(True)
